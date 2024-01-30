a3afe423adbe158decfc7d844a451011bc006950
DKK <DhivyaK@ocs-consulting.com>
2024-01-30 16:15:44 +0000

    rework on the use of api and logic

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# weather-Forecastify
-Weather dashboard
+# weather forecast Application
+
+## About
+
+The weather fore
+
+## Table of contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+    * [Screenshots](#screenshots)
+* [Credits](#credits)
+* [License](#license)
+
+## Installation
+
+The application is compatible with most commonly used web browsers.
+
+The published website is accessible at   <a href=""><img src="https://img.shields.io/badge/see-published-site"  alt="Live Site" /></a>
+
+Github code for the application is available at
+    <a href=""><img src="https://img.shields.io/badge/git-code-repository"  alt="github code repository" /></a>
+
+## Usage
+
+* Browse the weather forecast application using the URL mentioned in the above section.
+* The landing page should default with the weather information for a default city (example - London)
+* The weather data is displayed under two sections. One for the current day and a 5 day forecast information. Parameters like temperature, humidity, wind are mentioned in metric measurement.
+* Enter a city name in the search box provided and click on the 'search' button. The information on the right hand side of the page should load with current day weather for that city and a 5 day forecast information.
+* Notice that the search location history is recorded as buttons on the left side of the page. Click the location button of choice without having to type the same city again. The application should load the weather day again.
+
+### Screenshots
+
+The sample screen shots of the functionalities of application is available below.
+
+## License
+
+MIT License. Dhivya Kotha Kamalraj ©2024 All Rights Reserved.
+
+
+
+
--- assets/script/script.js
+++ assets/script/script.js
@@ -1,131 +1,153 @@
 const api = {
-    key : "388ac79e9a4d29966a74b4b19f53f87c",
-    url : "https://api.openweathermap.org/data/2.5/weather?q=",
-    forecasturl : "https://api.openweathermap.org/data/2.5/forecast?lat="
-}
+    key: "388ac79e9a4d29966a74b4b19f53f87c",
+    //url: "https://api.openweathermap.org/data/2.5/weather?q=",
+    latlongurl : "http://api.openweathermap.org/geo/1.0/direct?q=",
+    forecasturl : "http://api.openweathermap.org/data/2.5/forecast?units=metric&lat=",
+    //forecasturl: "https://api.openweathermap.org/data/2.5/forecast?lat=",
+  };
+  
+  const currentDate = dayjs().format("DD/MM/YYYY");
+  
+  //declare global vars
+  var citiesArr = [];
+  var longitude; 
+  var latitude;
+  
+ //get user input or default to the placeholder text  
+  var cityInput = $("#search-input").val().trim() || $("#search-input").attr("placeholder");
 
-const cityInput = $("#search-input");
-const searchButton = $(".search-button");
-const todaySection = $("#today");
-const forecastSection = $("#forecast");
-var queryURL;
-const currentDate = dayjs().format("DD/MM/YYYY");
-var defaultCity = cityInput.attr("placeholder");
-var citiesArr = [];
-
-//on page load, default the weather data
-
-$(window).on("load" , function(){
-   
-    buildLocationSearchList();
-    fetch(queryURL)
-    .then(function(response){
+   $(".search-button").on("click", onClickSearch);
+  
+  //Initialise local storage to hold search history
+  citiesArr = JSON.parse(localStorage.getItem("cityname")) || [];
+  
+  //get the latitude and longitude co-ordinates
+  var latlangqueryURL = api.latlongurl + cityInput + "&limit=5&appid=" + api.key;
+  
+  fetch(latlangqueryURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data)
+      if (data.length==0) {
+          
+        $(".city-name").text( "City not found. Please enter a valid city."
+        );
+        return;
+        }
+    //assign the search string with the value returned from API
+    cityInput = data[0].name;
+       
+     buildWeatherAPI (data);
+    
+    });
+  
+  //   function renderSearchHistory(city)
+  //   {
+  //     citiesArr = JSON.parse(localStorage.getItem("cityname")) || [];
+  //     $.each(citiesArr, function (index, item) {
+  //         $("#history").append(
+  //           $(
+  //             `<button type="submit" id="city" class="rounded-3 search-button form-control btn bg-primary text-white d-block p-2">${item.name}</button>`
+  //           )
+  //         );
+  //       });
+  //   }
+  
+  function buildWeatherAPI(responsedata)
+  {
+    longitude = responsedata[0].lon;
+    latitude = responsedata[0].lat;
+    var current = api.forecasturl + latitude + "&lon=" + longitude + "&appid=" + api.key;
+    var forecast = api.dailyforecasturl + latitude + "&lon=" + longitude + "&cnt=5" + "&appid=" + api.key;
+    console.log( "forecast " + current);
+    fetch(current)
+    .then (function(response){
         return response.json();
     })
-    .then (function(data){
-        getWeatherForCurrentDay(data);
-    }) 
-})
+    .then(function(data){
+        getCurrentDayWeather(data);
+        getWeatherForecast (data);
+    })
+  }
+    
+function getCurrentDayWeather(responsedata) {
+    $(".card-body").empty();
+    var temperature = responsedata.list[0].main.temp;
+    var wind = responsedata.list[0].wind.speed;
+    var humidity = responsedata.list[0].main.humidity;
+    var iconcode = responsedata.list[0].weather[0].icon;
+    var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+    $(".card-body").append( `<div class="d-flex flex-row"><h5 class="card-title" id="city">${ cityInput + " (" + currentDate + ")"}</h5><img id="wicon" src="${iconurl}" alt="Weather icon"></div>` );
+    $(".card-body").append(`<p class="card-text"  id="temperature">${ "Temperature : " + temperature + "°C"}</p>` );
+    $(".card-body").append(` <p class="card-text"  id="wind">${"Wind : " + wind + " KPH"}</p>`);
+    $(".card-body").append( `<p class="card-text"  id="humidity">${"Humidity : " + humidity + "%" }</p>`  );
+}
 
-//this function handles events where search button is clicked
-//$(".search-button").on("click", onClickSearch($(".search-button").text()));
-$(".search-button").on("click", onClickSearch);
-
-function onClickSearch(event){
+function getWeatherForecast(responsedata)
+{
+    console.log("inside forecast" + responsedata.list);
+}
+  
+  
+  //buildLocationSearchList();
+  
+  
+  //handle events where search button is clicked
+    function onClickSearch(event) {
     event.preventDefault();
 
     //clear the list and rebuild
     $("#history").empty();
-
-   var cityInput = $("#search-input").val().trim() ||  $("#search-input").attr("placeholder");
-    
-    if(cityInput != '')
-    {
-        queryURL = api.url + cityInput + "&appid=" + api.key;
-
-        fetch(queryURL)
-        .then(function(response){
-            return response.json();
+    if (cityInput != "") {
+    //  latlangqueryURL = api.latlongurl + cityInput + "&limit=5&appid=" + api.key;
+      
+      fetch(latlangqueryURL)
+        .then(function (response) {
+          return response.json();
         })
-        .then (function(data){
-            
-            if(data.cod ===200)
-            {   
-                let cityObj ={
-                    name : cityInput
-                }
-                //add the city just searched to the local storage
-               
-                citiesArr.push(cityObj)
-               // citiesArr = jQuery.unique(citiesArr);
-                saveCity();
-
-                //reload the search history
-                buildLocationSearchList();
-               
-                getWeatherForCurrentDay(data);
-            }
-        })
-       
+        .then(function (data) {
+          if (data.length==0) {
+            $(".city-name").text( "City not found. Please enter a valid city."
+            );
+            return;
+          }
+ 
+          let cityObj = {
+            name: cityInput,
+          };
+          //add the city just searched to the local storage
+          citiesArr.push(cityObj);
+  
+          saveCity();
+  
+          //reload the search history
+         // buildLocationSearchList();
+         buildWeatherAPI (data);
+          //getCurrentDayWeather(data);
+        });
     }
-}
-
-function saveCity(){
-    localStorage.setItem("cityname", JSON.stringify(citiesArr)); 
-}
-
-function buildLocationSearchList()
-{
-   
+  }
+  /*
+  function saveCity() {
+    localStorage.setItem("cityname", JSON.stringify(citiesArr));
+  }
+  
+  function buildLocationSearchList() {
     citiesArr = JSON.parse(localStorage.getItem("cityname")) || [];
     queryURL = api.url + defaultCity + "&appid=" + api.key;
-
-    $.each(citiesArr, function(index, item)
-    {
-        $("#history").append($(`<button type="submit" id="city" class="rounded-3 search-button form-control btn bg-primary text-white d-block p-2">${item.name}</button>`));
-        $("#"+ response.name).on("click", function(event){
-            event.preventDefault();
-            var cityName = index;
-
-           // getWeatherForCurrentDay(cityname, "existng");
-        })
-
-        
-    })
-
-    //bind the click event to button
-}
-
-function getWeatherForCurrentDay(responsedata)
-{
-    
-    var cardEle = $(".card-body");
-    $(".card-body").empty();
-    var longitude = responsedata.coord.lon;
-    var latitude = responsedata.coord.lat;
-    var queryForecastURL = api.forecasturl + latitude+ "&lon="+ longitude + "&appid=" + api.key;
-    var temp = (responsedata.main.temp - 273.15) * 1.80 + 32;
-    //display today weather
-    var cardTitleEle = $(`<h5 class="card-title" id="city">${responsedata.name + " (" + currentDate + ")"}</h5>`);
-    var cardTextTemp = $(`<p class="card-text"  id="temperature">${"Temp : " + temp.toFixed() + " C"}</p>`);
-    var cardTextWind = $(` <p class="card-text"  id="wind">${"Wind : " + responsedata.wind.speed + " KPH"}</p>`)
-    var cardTextHumidity = $(`<p class="card-text"  id="humidity">${"Humidity : " + responsedata.main.humidity + "%"}</p>`)
-    cardEle.append(cardTitleEle);
-    cardEle.append(cardTextTemp);
-    cardEle.append(cardTextWind);
-    cardEle.append(cardTextHumidity);
-
-    fetch(queryForecastURL)
-    .then(function(response){
-        return response.json();
-    })
-    .then (function(forecastdata){
-        console.log( "inside 2nd api" + forecastdata);
   
-        console.log(queryForecastURL);
-    })
-}
-
-
-
-
+    $.each(citiesArr, function (index, item) {
+      $("#history").append(
+        $(
+          `<button type="submit" id="city" class="rounded-3 search-button form-control btn bg-primary text-white d-block p-2">${item.name}</button>`
+        )
+      );
+    });
+  
+    //bind the click event to button
+  }
+  
+  */
+  
