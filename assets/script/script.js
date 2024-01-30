038ae7fe812042a1fa61974c0dfbc1c5db1ca8c9
DKK <DhivyaK@ocs-consulting.com>
2024-01-30 05:27:19 +0000

    code added to build the search list

--- assets/script/script.js
+++ assets/script/script.js
@@ -11,48 +11,34 @@ const forecastSection = $("#forecast");
 var queryURL;
 const currentDate = dayjs().format("DD/MM/YYYY");
 var defaultCity = cityInput.attr("placeholder");
-var cities = [];
+var citiesArr = [];
 
 //on page load, default the weather data
 
 $(window).on("load" , function(){
    
-    createCityList();
+    buildLocationSearchList();
     fetch(queryURL)
     .then(function(response){
         return response.json();
     })
     .then (function(data){
-        createCardData(data);
-    //   var cardTitleEle = $(`<h5 class="card-title" id="city">${data.name + "(" + currentDate + ")"}</h5>`);
-    //   var cardEle = $(".card-body");
-    //   var cardTextTemp = $(`<p class="card-text"  id="temperature">${"Temp : " + data.main.temp}</p>`);
-    //   var cardTextWind = $(` <p class="card-text"  id="wind">${"Wind : " + data.wind.speed + " KPH"}</p>`)
-    //   var cardTextHumidity = $(`<p class="card-text"  id="humidity">${"Humidity : " + data.main.humidity + "%"}</p>`)
-    //   cardEle.append(cardTitleEle);
-    //   cardEle.append(cardTextTemp);
-    //   cardEle.append(cardTextWind);
-    //   cardEle.append(cardTextHumidity);
-       
-     // $("#city").text(data.name + "(" + currentDate + ")");
-    // $("#temperature").text("Temp : " + data.main.temp);
-    // $("#wind").text("Wind : " + data.wind.speed + " KPH")
-    //$("#humidity").text("Humidity : " + data.main.humidity + "%")
-
+        getWeatherForCurrentDay(data);
     }) 
 })
 
 //this function handles events where search button is clicked
+//$(".search-button").on("click", onClickSearch($(".search-button").text()));
 $(".search-button").on("click", onClickSearch);
 
 function onClickSearch(event){
+    event.preventDefault();
 
     //clear the list and rebuild
     $("#history").empty();
-    event.preventDefault();
-    var cityInput = $("#search-input").val().trim() ||  $("#search-input").attr("placeholder");
+
+   var cityInput = $("#search-input").val().trim() ||  $("#search-input").attr("placeholder");
     
-    alert(cityInput);
     if(cityInput != '')
     {
         queryURL = api.url + cityInput + "&appid=" + api.key;
@@ -69,40 +55,61 @@ function onClickSearch(event){
                     name : cityInput
                 }
                 //add the city just searched to the local storage
-                cities.push(cityObj)
-                localStorage.setItem("cityname", JSON.stringify(cities));    
-                createCityList();
-                createCardData(data);
+               
+                citiesArr.push(cityObj)
+               // citiesArr = jQuery.unique(citiesArr);
+                saveCity();
+
+                //reload the search history
+                buildLocationSearchList();
+               
+                getWeatherForCurrentDay(data);
             }
         })
        
     }
 }
 
-function createCityList()
-{
-   
-    cities = JSON.parse(localStorage.getItem("cityname")) || [];
-    queryURL = api.url + defaultCity + "&appid=" + api.key;
-
-    for(let i=0 ; i <= cities.length; i++)
-    {
-      $("#history").append($(` <button type="submit" class="rounded-3 form-control btn bg-primary text-white  d-block p-2">${cities[i].name}</button>`));
-    }
+function saveCity(){
+    localStorage.setItem("cityname", JSON.stringify(citiesArr)); 
 }
 
-function createCardData(data)
+function buildLocationSearchList()
 {
-    console.log("data" + data);
+   
+    citiesArr = JSON.parse(localStorage.getItem("cityname")) || [];
+    queryURL = api.url + defaultCity + "&appid=" + api.key;
+
+    $.each(citiesArr, function(index, item)
+    {
+        $("#history").append($(`<button type="submit" id="city" class="rounded-3 search-button form-control btn bg-primary text-white d-block p-2">${item.name}</button>`));
+        $("#"+ response.name).on("click", function(event){
+            event.preventDefault();
+            var cityName = index;
+
+           // getWeatherForCurrentDay(cityname, "existng");
+        })
+
+        
+    })
+
+    //bind the click event to button
+}
+
+function getWeatherForCurrentDay(responsedata)
+{
+    
     var cardEle = $(".card-body");
     $(".card-body").empty();
-    var longitude = data.coord.lon;
-    var latitude = data.coord.lat;
+    var longitude = responsedata.coord.lon;
+    var latitude = responsedata.coord.lat;
     var queryForecastURL = api.forecasturl + latitude+ "&lon="+ longitude + "&appid=" + api.key;
-    var cardTitleEle = $(`<h5 class="card-title" id="city">${data.name + " (" + currentDate + ")"}</h5>`);
-    var cardTextTemp = $(`<p class="card-text"  id="temperature">${"Temp : " + data.main.temp}</p>`);
-    var cardTextWind = $(` <p class="card-text"  id="wind">${"Wind : " + data.wind.speed + " KPH"}</p>`)
-    var cardTextHumidity = $(`<p class="card-text"  id="humidity">${"Humidity : " + data.main.humidity + "%"}</p>`)
+    var temp = (responsedata.main.temp - 273.15) * 1.80 + 32;
+    //display today weather
+    var cardTitleEle = $(`<h5 class="card-title" id="city">${responsedata.name + " (" + currentDate + ")"}</h5>`);
+    var cardTextTemp = $(`<p class="card-text"  id="temperature">${"Temp : " + temp.toFixed() + " C"}</p>`);
+    var cardTextWind = $(` <p class="card-text"  id="wind">${"Wind : " + responsedata.wind.speed + " KPH"}</p>`)
+    var cardTextHumidity = $(`<p class="card-text"  id="humidity">${"Humidity : " + responsedata.main.humidity + "%"}</p>`)
     cardEle.append(cardTitleEle);
     cardEle.append(cardTextTemp);
     cardEle.append(cardTextWind);
@@ -113,14 +120,12 @@ function createCardData(data)
         return response.json();
     })
     .then (function(forecastdata){
-        console.log( forecastdata);
-
+        console.log( "inside 2nd api" + forecastdata);
+  
+        console.log(queryForecastURL);
     })
 }
 
-function createSearchList()
-{
-    
-}
+
 
 
